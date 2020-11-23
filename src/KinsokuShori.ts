3a76b2df7ef7e8e42916e0a20db21b4474f4f6fb
Fish <huang.yuwei@goodpatch.com>
2020-11-23 23:14:37 +0900

    :sparkles: use kinsoku-shori to fix the word-wrap issue

---
+++ src/KinsokuShori.ts
@@ -0,0 +1,30 @@
+// Line breaking rules in CJK (Kinsoku Shori)
+// Refer from https://en.wikipedia.org/wiki/Line_breaking_rules_in_East_Asian_languages
+const regexCannotStartZhCn = /[!%),.:;?\]}¢°·'""†‡›℃∶、。〃〆〕〗〞﹚﹜！＂％＇），．：；？！］｝～]/;
+const regexCannotEndZhCn = /[$(£¥·'"〈《「『【〔〖〝﹙﹛＄（．［｛￡￥]/;
+const regexCannotStartZhTw = /[!),.:;?\]}¢·–— '"•" 、。〆〞〕〉》」︰︱︲︳﹐﹑﹒﹓﹔﹕﹖﹘﹚﹜！），．：；？︶︸︺︼︾﹀﹂﹗］｜｝､]/;
+const regexCannotEndZhTw = /[([{£¥'"‵〈《「『〔〝︴﹙﹛（｛︵︷︹︻︽︿﹁﹃﹏]/;
+const regexCannotStartJaJp = /[)\]｝〕〉》」』】〙〗〟'"｠»ヽヾーァィゥェォッャュョヮヵヶぁぃぅぇぉっゃゅょゎゕゖㇰㇱㇲㇳㇴㇵㇶㇷㇸㇹㇺㇻㇼㇽㇾㇿ々〻‐゠–〜? ! ‼ ⁇ ⁈ ⁉・、:;,。.]/;
+const regexCannotEndJaJp = /[([｛〔〈《「『【〘〖〝'"｟«—...‥〳〴〵]/;
+const regexCannotStartKoKr = /[!%),.:;?\]}¢°'"†‡℃〆〈《「『〕！％），．：；？］｝]/;
+const regexCannotEndKoKr = /[$([{£¥'"々〇〉》」〔＄（［｛｠￥￦ #]/;
+
+const regexCannotStart = new RegExp(
+  `${regexCannotStartZhCn.source}|${regexCannotStartZhTw.source}|${regexCannotStartJaJp.source}|${regexCannotStartKoKr.source}`,
+);
+const regexCannotEnd = new RegExp(
+  `${regexCannotEndZhCn.source}|${regexCannotEndZhTw.source}|${regexCannotEndJaJp.source}|${regexCannotEndKoKr.source}`,
+);
+
+export const canBreakChars = function canBreakChars(
+  char: string,
+  nextChar?: string,
+) {
+  if (nextChar) {
+    // Line breaking rules in CJK (Kinsoku Shori)
+    if (regexCannotEnd.exec(char) || regexCannotStart.exec(nextChar)) {
+      return false;
+    }
+  }
+  return true;
+};
