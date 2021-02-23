47aa83429c9de272ad6c465cb6c89022ad0461fd
Fish <huang.yuwei@goodpatch.com>
2021-02-23 17:17:59 +0900

    :sparkles: sum width from cache data

--- src/BreakableChars.ts
+++ src/BreakableChars.ts
@@ -1,5 +1,12 @@
 const LATIN_REGEX = /[A-Za-z]/;
 
+export const sumTextWidthByCache = (
+  text: string,
+  cache: { [key in string]: number },
+) => {
+  return text.split('').reduce((sum: number, c) => sum + cache[c], 0);
+};
+
 export const findBreakableIndex = (line: string): number => {
   for (let i = line.length - 1; i >= 0; i--) {
     if (!LATIN_REGEX.test(line[i])) return i + 1;
