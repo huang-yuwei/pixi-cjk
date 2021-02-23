cf347fc00a8d39414aae487c3a7af86bb305d70f
Fish <huang.yuwei@goodpatch.com>
2021-02-23 17:19:06 +0900

    :sparkles: trim word to not latin word including space

--- src/BreakableChars.ts
+++ src/BreakableChars.ts
@@ -1,5 +1,17 @@
 const LATIN_REGEX = /[A-Za-z]/;
 
+export const trimToBreakable = (prev: string[]): string[] => {
+  const next = [...prev];
+  const prevLine = next[next.length - 2];
+
+  const indexToTrim = findBreakableIndex(prevLine);
+  if (indexToTrim === -1 || !prevLine) return next;
+
+  next[next.length - 1] = prevLine.slice(indexToTrim, prevLine.length);
+  next[next.length - 2] = prevLine.slice(0, indexToTrim);
+  return next;
+};
+
 export const sumTextWidthByCache = (
   text: string,
   cache: { [key in string]: number },
