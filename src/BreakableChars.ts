68b6f912a6539dc0b3f255cc90ebda29206f46d5
Fish <huang.yuwei@goodpatch.com>
2021-02-23 20:07:31 +0900

    :+1: improve breakable index by specifying the logic

--- src/BreakableChars.ts
+++ src/BreakableChars.ts
@@ -1,3 +1,5 @@
+import * as PIXI from 'pixi.js';
+
 const LATIN_REGEX = /[a-zA-Z0-9\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff!"#$%&'()*+,-./:;]/;
 
 export const canBreakInLastChar = (char: string | undefined): boolean => {
@@ -9,11 +11,17 @@ export const trimToBreakable = (prev: string[]): string[] => {
   const next = [...prev];
   const prevLine = next[next.length - 2];
 
-  const indexToTrim = findBreakableIndex(prevLine);
-  if (indexToTrim === -1 || !prevLine) return next;
+  const index = findBreakableIndex(prevLine);
+  if (index === -1 || !prevLine) return next;
 
-  next[next.length - 1] = prevLine.slice(indexToTrim, prevLine.length);
-  next[next.length - 2] = prevLine.slice(0, indexToTrim);
+  const trimmedChar = prevLine.slice(index, index + 1);
+  const isTrimmedWithSpace = PIXI.TextMetrics.isBreakingSpace(trimmedChar);
+
+  const trimFrom = index + 1;
+  const trimTo = index + (isTrimmedWithSpace ? 0 : 1);
+  next[next.length - 1] += prevLine.slice(trimFrom, prevLine.length);
+  next[next.length - 2] = prevLine.slice(0, trimTo);
+
   return next;
 };
 
@@ -26,7 +34,7 @@ export const sumTextWidthByCache = (
 
 export const findBreakableIndex = (line: string): number => {
   for (let i = line.length - 1; i >= 0; i--) {
-    if (!LATIN_REGEX.test(line[i])) return i + 1;
+    if (!LATIN_REGEX.test(line[i])) return i;
   }
   return -1;
 };
